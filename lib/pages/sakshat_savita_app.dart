79c9ea5e631287f9ae7b76f031dfc831ca222adf
rakeshcjadav <rakeshcjadav@gmail.com>
2025-08-29 12:32:10 +0530

    Added Firebase plugin

--- lib/pages/sakshat_savita_app.dart
+++ lib/pages/sakshat_savita_app.dart
@@ -1,9 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'homepage.dart';
+import '../firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  // Initialize Firebase
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const SakshatSavitaApp());
 }
 
